b99d71b0ec4d328ccc57f5bbc274df4c5c3b1a50
Steven Cable <steven.cable12@gmail.com>
2017-03-25 11:18:29 -1000

    clockwise and counterclockwise passing

--- MatrixRotator.js
+++ MatrixRotator.js
@@ -12,6 +12,7 @@ const Direction = require("./Direction").Direction;
  */
 module.exports = class MatrixRotator {
   constructor( matrix ) {
+
     this.matrix = matrix;
   }
 
@@ -19,7 +20,33 @@ module.exports = class MatrixRotator {
   //      v        or Direction.CCW
   rotate (direction) {
     // do work here
+    // let PA = this.matrix;
+    let newPA = [];
+    if(direction === Direction.CW ){
+  
 
+    for (var i = 0; i <= this.matrix.length-1; i++){
+      let newCA = [];
+      newPA.push(newCA);
+      
+     for (var j = this.matrix.length-1; j >= 0; j--){ 
+        newCA.push(this.matrix[j][i]);
+     }
+    }
+  }else{
+
+    for (var k = 0; k <= this.matrix.length-1; k++){
+     let newCA = [];
+      newPA.unshift(newCA);
+      
+     for (var l = this.matrix.length-1; l >= 0; l--){ 
+        newCA.unshift(this.matrix[l][k]);
+     }
+    }
+
+  }
+
+  this.matrix = newPA;
     // must be a valid Direction, see Direction.js
 
   }
